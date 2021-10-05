0dac622604129aadaf90e2cd69872cae5c08ac01
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-10-05 16:36:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('pose is initialised');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function draw(){
+    background('#969A97');
+}
