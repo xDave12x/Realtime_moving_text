24285253f1058618171cfbec094aec79be15e13b
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-10-07 16:52:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,8 +17,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY="+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftwristX="+leftWristX+"rightwristX="+rightWristX+"difference="+difference);
     }
 }
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML="The font size of the text will be="+difference+"px";
+    fill('#f90093');
+    textSize(difference);
+    text('Dave',noseX,noseY);
 }
